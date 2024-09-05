adf1bbf5fc4badd28eceecd14e9093d144399e72
سارة جمال <soso_j_kh@hotmail.com>
2024-09-05 14:43:59 +0300

    Add project files.

---
+++ Components/Models/CitiesRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace ServerManagment.Components.Models
+{
+    public class CitiesRepository
+    {
+
+        public static List<string> Cities = new List<string>()
+        {
+            "Toronto",
+            "Montreal",
+            "Ottawa",
+            "Calgary",
+            "Halifax"
+        };
+
+        public static List<string> GetCeties() => Cities;
+    }
+}
